f6547f81bc4394f40858d62dce06171b90de36bc
red-mat <jesus.ing.rodriguez@gmail.com>
2022-09-27 20:00:17 -0300

    feat(App): implement container in App

--- mod3-challenge01/src/App.js
+++ mod3-challenge01/src/App.js
@@ -5,14 +5,17 @@ import CharacterSelected from './components/character-selected/characterSelected
 import CharacterList from './components/character-list/characterList';
 
 import default_character from './assets/default-character.png';
+import Container from './components/container/Container';
 
 function App() {
   const [character_img, setImageURL] = useState(default_character);
 
   return (
     <div className='App'>
-      <CharacterSelected characterImg={character_img} />
-      <CharacterList setImageURL={setImageURL} />
+      <Container>
+        <CharacterSelected characterImg={character_img} />
+        <CharacterList setImageURL={setImageURL} />
+      </Container>
     </div>
   );
 }
