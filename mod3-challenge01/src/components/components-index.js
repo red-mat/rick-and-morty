489534c3247668f33ff751d4938a6dac47adfbff
red-mat <jesus.ing.rodriguez@gmail.com>
2022-09-29 22:40:46 -0300

    feat(components): add index-components

---
+++ mod3-challenge01/src/components/components-index.js
@@ -0,0 +1,14 @@
+import CharacterCard from "./character-card/characterCard";
+import CharacterList from "./character-list/characterList";
+import CharacterSelected from "./character-selected/characterSelected";
+import Container from "./container/Container";
+import LoadingScreen from "./loading-screen/loadingScreen";
+
+
+export {
+    CharacterCard,
+    CharacterList,
+    CharacterSelected,
+    Container,
+    LoadingScreen
+};
