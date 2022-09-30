2e1921a046a5525f6ea235c52bc64e24ed6874ed
red-mat <jesus.ing.rodriguez@gmail.com>
2022-09-29 22:48:54 -0300

    fix(components): rename component-container to container-component

--- mod3-challenge01/src/components/components-index.js
+++ mod3-challenge01/src/components/components-index.js
@@ -1,7 +1,7 @@
 import CharacterCard from "./character-card/characterCard";
 import CharacterList from "./character-list/characterList";
 import CharacterSelected from "./character-selected/characterSelected";
-import Container from "./container/Container";
+import Container from "./container-component/Container";
 import LoadingScreen from "./loading-screen/loadingScreen";
 
 
