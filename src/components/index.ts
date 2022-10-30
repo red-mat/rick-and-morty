df2a93302932c3a56920008f25020019b082b9d8
red-mat <jesus.ing.rodriguez@gmail.com>
2022-10-30 02:12:50 -0300

    chore(components): rename components index

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,15 +1,7 @@
 import CharacterCard from './character-card/characterCard';
 import CharacterList from './character-list/characterList';
-import CharacterSelected from './character-selected/characterSelected';
 import Container from './container-component/Container';
 import HeaderApp from './header_app/header-app';
 import LoadingScreen from './loading-screen/loadingScreen';
 
-export {
-  HeaderApp,
-  CharacterCard,
-  CharacterList,
-  CharacterSelected,
-  Container,
-  LoadingScreen,
-};
+export { HeaderApp, CharacterCard, CharacterList, Container, LoadingScreen };
