0881ab4fd08a27db160dce80fce8f5bd6faf2edd
red-mat <jesus.ing.rodriguez@gmail.com>
2022-11-05 17:10:26 -0300

    refactor: App

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,17 @@
 import './css/App.css';
 
-import { Route, Routes, Navigate } from 'react-router-dom';
-
-import { useState } from 'react';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { Home, Navigation } from './routes/index';
 
-import useCharacters from './modules/rick_and_morty_api/hooks/characters';
-
-const HOME_PATH = '/rick-and-morty';
-
 function App(): JSX.Element {
-  const [characterSelected, setImageURL] = useState('');
-  const [characterList, isLoading] = useCharacters();
+  const HOME_PATH = '/rick-and-morty';
 
   return (
     <div className='App'>
       <Routes>
         <Route path='/' element={<Navigate to={HOME_PATH} />} />
-        <Route
-          path={HOME_PATH}
-          element={<Navigation character={characterSelected} />}
-        >
-          <Route
-            index
-            element={
-              <Home
-                characterList={characterList}
-                setImageURL={setImageURL}
-                isLoading={isLoading}
-              />
-            }
-          />
+        <Route path={HOME_PATH} element={<Navigation />}>
+          <Route index element={<Home />} />
         </Route>
       </Routes>
     </div>
