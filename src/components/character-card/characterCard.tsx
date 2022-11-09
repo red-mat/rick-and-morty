9397c0d9f755a41accbb9e3f968fe6724673225f
red-mat <jesus.ing.rodriguez@gmail.com>
2022-11-08 22:59:24 -0300

    feature(component): udpate style character card

--- src/components/character-card/characterCard.tsx
+++ src/components/character-card/characterCard.tsx
@@ -3,19 +3,17 @@ import './characterCard.css';
 import { Container } from '../index';
 import { iCharacterApiResult } from '../../modules/rick_and_morty_api/services/types';
 
-type tOneClick = () => void;
 interface iProps {
   character: iCharacterApiResult;
-  onClick: tOneClick;
 }
 
-function CharacterCard({ character, onClick }: iProps): JSX.Element {
+function CharacterCard({ character }: iProps): JSX.Element {
   return (
-    <Container onClick={onClick} modifier='character-card'>
+    <Container modifier='character-card'>
       <img src={character.image} alt='' className='characterCard__avatar' />
-      <p>{character.name}</p>
-      <p>{character.species}</p>
-      <p>{character.status}</p>
+      <p className='character-card__text'>{character.name}</p>
+      <p className='character-card__text'>{character.species}</p>
+      <p className='character-card__text'>{character.status}</p>
     </Container>
   );
 }
