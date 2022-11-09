354d3787440d59c931463d3e8e6c40953e24cf4d
red-mat <jesus.ing.rodriguez@gmail.com>
2022-11-08 23:02:24 -0300

    feature(component): add style image container

--- src/components/image-container/image-container.tsx
+++ src/components/image-container/image-container.tsx
@@ -1,12 +1,19 @@
+import './image-container.css';
 import { Container } from '../index';
 
 interface iProps {
   src: string;
+  alt?: string;
+  modifier?: string;
 }
-export default function ImageContainer(props: iProps): JSX.Element {
+export default function ImageContainer({
+  src,
+  alt,
+  modifier,
+}: iProps): JSX.Element {
   return (
-    <Container>
-      <img src={props.src} alt='' />
+    <Container modifier={modifier}>
+      <img className='container__img' src={src} alt={alt} />
     </Container>
   );
 }
