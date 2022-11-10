a932f5bbba6f4493430baa23cadda1c2e7e499b7
red-mat <jesus.ing.rodriguez@gmail.com>
2022-11-10 20:54:32 -0300

    fix(components): adaptive eslint container-component

--- src/components/container-component/Container.tsx
+++ src/components/container-component/Container.tsx
@@ -1,16 +1,25 @@
+import React from 'react';
 import './Container.css';
 
-const Container = ({
+type Props = Partial<iProps>;
+interface iProps {
+  children: React.ReactNode;
+  modifier: string;
+  style: React.CSSProperties;
+  onClick: () => void;
+}
+
+function Container({
   children,
   modifier = '',
   style = {},
   onClick = () => {},
-}) => {
+}: Props): JSX.Element {
   return (
     <div className={`Container ${modifier}`} style={style} onClick={onClick}>
       {children}
     </div>
   );
-};
+}
 
 export default Container;
